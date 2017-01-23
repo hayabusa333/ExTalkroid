b4436dcf407d169b7d51162075a3c4b96ae12106
Junnichi Kitta <soiru_megane12@hotmail.com>
2017-01-23 19:01:55 +0900

    add init

--- lib/ex_talkroid.ex
+++ lib/ex_talkroid.ex
@@ -1,11 +1,18 @@
 defmodule ExTalkroid do
   def main(args) do
+    init()
     hoge = "テスト"
     conf = Mix.Config.read!("config/config.exs")
     System.cmd("#{conf[:softalk][:dir_path]}/SofTalkw.exe", ["/S:100", "/V:100", "/W:#{hoge}"])
   end
 
   def init do
-
+    conf = Mix.Config.read!("config/config.exs")
+    case System.cmd("#{conf[:softalk][:dir_path]}/SofTalkw.exe", ["/X:1"]) do
+      {"", 0} ->
+        :ok
+      _ ->
+        :error
+    end
   end
 end
