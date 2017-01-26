4dc84d5553960bd4a1d6c11829bec191b54bd04d
Junnichi Kitta <soiru_megane12@hotmail.com>
2017-01-27 08:22:05 +0900

    delete main method

--- lib/ex_talkroid.ex
+++ lib/ex_talkroid.ex
@@ -1,12 +1,5 @@
 defmodule ExTalkroid do
 
-  def main(args) do
-    init()
-    hoge = "テスト"
-    conf = Mix.Config.read!("config/config.exs")
-    System.cmd("#{conf[:softalk][:dir_path]}/SofTalkw.exe", ["/S:#{conf[:softalk][:speed]}", "/V:#{conf[:softalk][:volume]}", "/W:#{hoge}"])
-  end
-
   def talk(comment) do
     init()
     ExTalkroid.Softalk.talk(comment)
