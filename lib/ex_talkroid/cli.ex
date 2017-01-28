7d5eac70229a35be579ed0c4de651b7d4fed4341
Junnichi Kitta <soiru_megane12@hotmail.com>
2017-01-28 09:37:50 +0900

    add parse args

--- lib/ex_talkroid/cli.ex
+++ lib/ex_talkroid/cli.ex
@@ -1,7 +1,15 @@
 defmodule ExTalkroid.Cli do
   def main(args) do
+    options = parse_args(args)
     ExTalkroid.Softalk.init()
-    comment = "テスト"
+    comment = options[:comment]
     ExTalkroid.Softalk.talk(comment)
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(args,
+      switches: [comment: :string]
+    )
+    options
   end
 end
